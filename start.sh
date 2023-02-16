dad292bb9da21056a28c073cee860c6caa1b9be3
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-02-16 23:56:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sneak2000/req-repo-2023 /Elsa
+  git clone https://github.com/Sneak2000/req-repo-2023.git /req-repo-2023
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Elsa
+  git clone $UPSTREAM_REPO /req-repo-2023
 fi
-cd /Elsa
+cd /req-repo-2023
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜....🧞‍♂️"
 python3 bot.py
